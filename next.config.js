74e16f26508c10fe912bbd2bdb237967083c3d95
bconclub <bconclubx@gmail.com>
2026-02-01 11:10:55 +0530

    Update next.config.js to allow embedding from pilot.windchasers.in

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://pilot.windchasers.in"
+          }
+        ]
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
